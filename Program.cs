9f33ac6040b3dd3640aa3f5f6f52799cbb4525d7
ALLUK <lam83@bk.ru>
2024-01-09 20:07:39 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassBuilding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Building house = new Building();
+
+            int quntityHouseAreaPerPerson = house.AreaPerPerson(60, 5, 2);
+            Console.WriteLine("Количество площади в доме на 1 жильца равно " + quntityHouseAreaPerPerson + " кв.м.");
+
+            Console.WriteLine();
+
+            Building office = new Building();
+            int quntityOfficeAreaPerPerson = office.AreaPerPerson(120, 24, 5);
+            Console.WriteLine("Количество площади в oфисе на 1 служащего равно " + quntityOfficeAreaPerPerson + " кв.м.");
+
+            Console.ReadLine();
+
+        }
+    }
+    class Building
+    {
+
+        public int AreaPerPerson(int area, int occupants, int floors)
+        {
+
+            int areaPerPerson = (area*floors) / occupants;
+            return areaPerPerson;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleAppClassBuilding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleAppClassBuilding")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("051c5d82-493d-458c-894b-0c416e4fbd64")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
